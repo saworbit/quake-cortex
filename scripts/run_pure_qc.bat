9c2f319333c1254bae1a45459fbb1ef18360ab17
Shane Wall <shane.wall@gmail.com>
2025-12-31 20:35:49 +1100

    Fix pure mode binds, bot roaming, and respawn delay

--- scripts/run_pure_qc.bat
+++ scripts/run_pure_qc.bat
@@ -31,7 +31,7 @@ set EXTRA_ARGS=%*
 if "%~1"=="" set EXTRA_ARGS=+set deathmatch 1 +map dm3
 
 set PURE_FLAGS=+set cortex_pure_mode 1 +set cortex_bot_enable 1 +set cortex_spawn_bot 1 +set pr_no_playerphysics 0 +exec autoexec.cfg
-set BIND_FLAGS=+bind w +forward +bind a +moveleft +bind s +back +bind d +moveright +bind SPACE +jump +bind SHIFT +speed +bind CTRL +attack +bind MOUSE1 +attack +bind MOUSE2 +mlook +set cl_forwardspeed 400 +set cl_backspeed 400 +set cl_sidespeed 400 +set cl_upspeed 200
+set BIND_FLAGS=+bind w +forward +bind a +moveleft +bind s +back +bind d +moveright +bind SPACE +jump +bind SHIFT +speed +bind CTRL +attack +bind MOUSE1 +attack +bind MOUSE2 +mlook +set cl_forwardspeed 400 +set cl_backspeed 400 +set cl_sidespeed 350 +set cl_upspeed 200 +set cl_movespeedkey 2
 
 echo [2/2] Launching pure Cortex client...
 pushd "%~dp0\\..\\Game" >nul 2>&1
